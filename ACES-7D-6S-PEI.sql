efed7dd74e6a6f0d340dca80d6014c3be3d04e73
Cameron Wade <cameron@sutubra.ca>
2023-02-16 10:50:18 -0400

    Add Min and MaxNewCapacityGroup constraint

--- ACES-7D-6S-PEI.sql
+++ ACES-7D-6S-PEI.sql
@@ -481,6 +481,28 @@ CREATE TABLE IF NOT EXISTS "MaxNewCapacity" (
 	FOREIGN KEY("tech") REFERENCES "technologies"("tech"),
 	PRIMARY KEY("regions","periods","tech")
 );
+CREATE TABLE IF NOT EXISTS "MinNewCapacityGroup" (
+	"regions"	text,
+	"periods"	integer,
+	"tech"	text,
+	"minnewcap"	real,
+	"minnewcap_units"	text,
+	"minnewcap_notes"	text,
+	PRIMARY KEY("regions","periods","tech"),
+	FOREIGN KEY("periods") REFERENCES "time_periods"("t_periods"),
+	FOREIGN KEY("tech") REFERENCES "technologies"("tech")
+);
+CREATE TABLE IF NOT EXISTS "MaxNewCapacityGroup" (
+	"regions"	text,
+	"periods"	integer,
+	"tech"	text,
+	"maxnewcap"	real,
+	"maxnewcap_units"	text,
+	"maxnewcap_notes"	text,
+	PRIMARY KEY("regions","periods","tech"),
+	FOREIGN KEY("periods") REFERENCES "time_periods"("t_periods"),
+	FOREIGN KEY("tech") REFERENCES "technologies"("tech")
+);
 CREATE TABLE IF NOT EXISTS "MinActivityGroup" (
 	"regions"	text,
 	"t_periods"	integer,
